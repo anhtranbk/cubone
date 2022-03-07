660ceb00bbf3bb327e87e6b8d07b80713927d805
tjeubaoit <anhtn.bk@gmail.com>
2022-03-07 21:15:18 +0700

    refactor message entities

--- message.go
+++ message.go
@@ -1,18 +1,18 @@
 package cubone
 
-// WebSocket client request message
-type WebSocketClientMessage struct {
+// WSClientMessage client request message
+type WSClientMessage struct {
 	Type string      `json:"type"`
 	Data interface{} `json:"data"`
 }
 
-// WebSocket handler response message
-type WebSocketServerMessage struct {
+// WSServerMessage handler response message
+type WSServerMessage struct {
 	ID   string      `json:"id"`
 	Data interface{} `json:"data"`
 }
 
-type OnsiteMessage struct {
+type DeliveryMessage struct {
 	Endpoint string
 	Data     interface{}
 }
