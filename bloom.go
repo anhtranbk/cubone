a867b9d6eab9f0df38f850c0b64f94587764c12c
tjeubaoit <anhtn.bk@gmail.com>
2022-03-13 22:04:28 +0700

    feat: add in-memory bloom filter impelementation (for testing)

--- bloom.go
+++ bloom.go
@@ -13,5 +13,32 @@ type RedisBloomFilter struct {
 
 }
 
+func NewInmemoryBloomFilter() *InmemoryBloomFilter {
+	return &InmemoryBloomFilter{
+		db: make(map[string]byte),
+	}
+}
 
+//InmemoryBloomFilter for development purpose
+type InmemoryBloomFilter struct {
+	db map[string]byte
+}
+
+func (i InmemoryBloomFilter) Exists(key string, item string) (bool, error) {
+	_, found := i.db[key + item]
+	return found, nil
+}
+
+func (i InmemoryBloomFilter) Add(key string, item string) error {
+	i.db[key + item] = 1
+	return nil
+}
+
+func (r RedisBloomFilter) Exists(key string, item string) (bool, error) {
+	panic("implement me")
+}
+
+func (r RedisBloomFilter) Add(key string, item string) error {
+	panic("implement me")
+}
 
