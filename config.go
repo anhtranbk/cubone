dc5722e8d641dbb9db915364c711ef820cece577
tjeubaoit <anhtn.bk@gmail.com>
2022-03-19 10:18:09 +0700

    feat: expand config

--- config.go
+++ config.go
@@ -3,4 +3,17 @@ package cubone
 type Config struct {
 	MessageRetryMaxTimeout int64
 	MessageRetryDelay      int64
+	GorillaWS              GorillaWsConfig
+	HTTPServer             HTTPServerConfig
+}
+
+type GorillaWsConfig struct {
+	ReadBufferSize  int
+	WriteBufferSize int
+}
+
+type HTTPServerConfig struct {
+	Address              string
+	ReadTimeoutInSecond  int
+	WriteTimeoutInSecond int
 }
