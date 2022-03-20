d9215fc630b0037349bce975b49defcdc3bb10e6
tjeubaoit <anhtn.bk@gmail.com>
2022-03-20 11:24:17 +0700

    feat: add api handler

--- server.go
+++ server.go
@@ -1,7 +1,9 @@
 package cubone
 
 import (
+	"encoding/json"
 	"github.com/gorilla/mux"
+	"io/ioutil"
 	"net/http"
 	"time"
 )
@@ -52,17 +54,68 @@ func (s Server) Serve() error {
 
 // /register/{client_id}/{x_client_id}/{x_client_access_token}
 func (h handler) createWebSocket(w http.ResponseWriter, r *http.Request) {
-	panic("implement me")
+	conn, err := h.wsServer.NewConnection(w, r)
+	if err != nil {
+		writeErrorResponse(w, http.StatusInternalServerError, "Could not create websocket connection")
+		log.Errorw("Could not create websocket connection", "error", err.Error())
+		return
+	}
+
+	if ok := handleRequestAuthentication(w, r); !ok {
+		return
+	}
+
+	params := mux.Vars(r)
+	clientId := params["client_id"]
+	err = h.onsiteService.ConnectClient(clientId, conn)
+	if err != nil {
+		writeErrorResponse(w, http.StatusInternalServerError, "Could not create websocket connection")
+		log.Errorw("Could not create websocket connection", "error", err.Error())
+	}
 }
 
 // /deregister/{client_id}/{x_client_id}/{x_client_access_token}
 func (h handler) removeWebSocket(w http.ResponseWriter, r *http.Request) {
-	panic("implement me")
+	if ok := handleRequestAuthentication(w, r); !ok {
+		return
+	}
+
+	params := mux.Vars(r)
+	clientId := params["client_id"]
+	err := h.onsiteService.DisconnectClient(clientId)
+	if err != nil {
+		writeErrorResponse(w, http.StatusInternalServerError, "Could not remove websocket connection")
+		log.Errorw("Could not remove websocket connection", "error", err.Error())
+
+	}
 }
 
 // /internal/onsite/trigger
 func (h handler) trigger(w http.ResponseWriter, r *http.Request) {
-	panic("implement me")
+	reader, err := r.GetBody()
+	if err != nil {
+		writeErrorResponse(w, http.StatusBadRequest, "Could not read body request")
+		return
+	}
+	buffer, err := ioutil.ReadAll(reader)
+	if err != nil {
+		writeErrorResponse(w, http.StatusBadRequest, "Could not read body request")
+		return
+	}
+
+	var message DeliveryMessage
+	err = json.Unmarshal(buffer, &message)
+	if err != nil {
+		writeErrorResponse(w, http.StatusBadRequest, "Could not parse body request")
+		return
+	}
+
+	err = h.onsiteService.PublishMessage(&message)
+	if err != nil {
+		writeErrorResponse(w, http.StatusInternalServerError, "handler could not publish message")
+		log.Errorw("Could not publish message", "error", err.Error())
+		return
+	}
 }
 
 func handleRequestAuthentication(w http.ResponseWriter, r *http.Request) bool {
