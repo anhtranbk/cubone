077d0a42f68f43b84c1ea4dc046935b690c86427
tjeubaoit <anhtn.bk@gmail.com>
2022-03-23 22:01:05 +0700

    feat: add main entry point

---
+++ cmd/app/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/anhtranbk/cubone"
+	"log"
+)
+
+func main() {
+	server, err := cubone.NewServer(cubone.Config{
+		MessageRetryMaxTimeout: 5,
+		MessageRetryDelay:      5,
+		GorillaWS: cubone.GorillaWsConfig{
+			ReadBufferSize:  8192,
+			WriteBufferSize: 8192,
+		},
+		HTTPServer: cubone.HTTPServerConfig{
+			Address:              "localhost:11053",
+			ReadTimeoutInSecond:  5,
+			WriteTimeoutInSecond: 5,
+		},
+	})
+	if err != nil {
+		log.Fatalln("Server error", err.Error())
+	}
+
+	log.Fatal(server.Serve())
+}
