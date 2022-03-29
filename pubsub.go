25b506adab7c95dc06d50531dfddab6925a33676
tjeubaoit <anhtn.bk@gmail.com>
2022-03-29 10:35:12 +0700

    fix: communicating with pubsub via channel

--- pubsub.go
+++ pubsub.go
@@ -1,7 +1,8 @@
 package cubone
 
+import "time"
+
 type PubSubMessage struct {
-	Type    string
 	Channel string
 	Data    interface{}
 }
@@ -11,34 +12,50 @@ type Publisher interface {
 }
 
 type Subscriber interface {
-	Subscribe(channel string) error
-	GetMessage(timeout int64) (*PubSubMessage, error)
+	Subscribe(channels ...string) error
+	Channel() <-chan *PubSubMessage
 }
 
 type FakePubSub struct {
+	ch chan *PubSubMessage
 }
 
-func (f FakePubSub) Subscribe(channel string) error {
+func NewFakePubSub() *FakePubSub {
+	fps := &FakePubSub{
+		ch: make(chan *PubSubMessage),
+	}
+	go fps.genFakeMessages()
+	return fps
+}
+
+func (f *FakePubSub) Subscribe(channels ...string) error {
 	return nil
 }
 
-func (f FakePubSub) GetMessage(timeout int64) (*PubSubMessage, error) {
-	return &PubSubMessage{
-		Type:    "message",
-		Channel: OnsiteChannel,
-		Data: struct {
-			name    string
-			age     int
-			address string
-		}{
-			name:    "nhat anh",
-			age:     33,
-			address: "q7, hcm",
-		},
-	}, nil
+func (f *FakePubSub) Channel() <-chan *PubSubMessage {
+	return f.ch
 }
 
-func (f FakePubSub) Publish(channel string, data interface{}) error {
+func (f *FakePubSub) Publish(channel string, data interface{}) error {
 	log.Debugw("message published", "channel", channel, "data", data)
 	return nil
+}
+
+func (f *FakePubSub) genFakeMessages() {
+	for {
+		msg := &PubSubMessage{
+			Channel: OnsiteChannel,
+			Data: struct {
+				name string
+				age  int
+				addr string
+			}{
+				name: "nhat anh",
+				age:  32,
+				addr: "q7, hcm",
+			},
+		}
+		f.ch <- msg
+		time.Sleep(time.Second)
+	}
 }
